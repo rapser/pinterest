118bc9196f8a1205d53be5d78d284173226dd8d7
Miguel Tomairo <matomairo@gmail.com>
2020-03-22 20:13:50 -0500

    first commit

---
+++ Facebook+Research/PinterestLayout.swift
@@ -0,0 +1,107 @@
+//
+//  PinterestLayout.swift
+//  Pinterest
+//
+//  Created by miguel tomairo on 3/22/20.
+//  Copyright © 2020 Rapser. All rights reserved.
+//
+
+import UIKit
+
+protocol PinterestLayoutDelegate: class {
+    
+    func collectionView(collectionView: UICollectionView, heightForPhotoAt indexPath: IndexPath, with width: CGFloat) -> CGFloat
+    
+    func collectionView(collectionView: UICollectionView, heightForCaptionAt indexPath: IndexPath, with width: CGFloat) -> CGFloat
+}
+
+class PinterestLayout: UICollectionViewLayout {
+    
+    var controller : NewsfeedCollectionViewController?
+    var numberOfColumns: CGFloat = 2
+    var cellPadding: CGFloat = 5.0
+    weak var delegate: PinterestLayoutDelegate?
+    
+    private var contentHeight: CGFloat = 0.0
+    private var contentWidth: CGFloat {
+        let insets = collectionView!.contentInset
+        return (collectionView!.bounds.width - (insets.left + insets.right))
+    }
+    
+    private var attributesCache = [PinterestLayoutAttributes]()
+    
+    override func prepare() {
+        
+        if attributesCache.isEmpty {
+            let columnWidth = contentWidth / numberOfColumns
+            var xOffsets = [CGFloat]()
+            for column in 0 ..< Int(numberOfColumns) {
+                xOffsets.append(CGFloat(column) * columnWidth)
+            }
+            
+            var column = 0
+            var yOffsets = [CGFloat](repeating: 0, count: Int(numberOfColumns))
+
+            for item in 0 ..< collectionView!.numberOfItems(inSection: 0) {
+                let indexPath = IndexPath(item: item, section: 0)
+                
+                // calculate the frame
+                let width = columnWidth - cellPadding * 2
+                
+                let photoHeight: CGFloat = (delegate?.collectionView(collectionView: collectionView!, heightForPhotoAt: indexPath, with: width))!
+                let captionHeight: CGFloat = (delegate?.collectionView(collectionView: collectionView!, heightForCaptionAt: indexPath, with: width))!
+                
+                let height = cellPadding + photoHeight + captionHeight
+                
+                let frame = CGRect(x: xOffsets[column], y: yOffsets[column], width: columnWidth, height: height)
+                
+                let insetFrame = frame.insetBy(dx: cellPadding, dy: cellPadding)
+                
+                let attributes = PinterestLayoutAttributes(forCellWith: indexPath)
+                attributes.photoHeight = photoHeight
+                attributes.frame = insetFrame
+                attributesCache.append(attributes)
+                
+                // update column
+                contentHeight = max(contentHeight, frame.maxY)
+                yOffsets[column] = yOffsets[column] + height
+                
+                if column >= Int(numberOfColumns - 1) {
+                    column = 0
+                }else {
+                    column += 1
+                }
+            }
+        }
+        
+    }
+    
+    override var collectionViewContentSize: CGSize {
+        return CGSize(width: contentWidth, height: contentHeight)
+    }
+    
+    override func layoutAttributesForElements(in rect: CGRect) -> [UICollectionViewLayoutAttributes]? {
+        
+        var layoutAttributes = [UICollectionViewLayoutAttributes]()
+        
+        for attributes in attributesCache {
+            if attributes.frame.intersects(rect) {
+                layoutAttributes.append(attributes)
+            }
+        }
+        
+        return layoutAttributes
+    }
+}
+
+class PinterestLayoutAttributes: UICollectionViewLayoutAttributes {
+    
+    var photoHeight: CGFloat = 0.0
+
+    override func copy(with zone: NSZone? = nil) -> Any {
+        let copy = super.copy(with: zone) as! PinterestLayoutAttributes
+        copy.photoHeight = photoHeight
+        return copy
+    }
+    
+}
